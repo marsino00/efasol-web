67b4ae6fb63a3501ca4ad88d1b573975e6a754ea
marsino00 <rogermarsino@gmail.com>
2025-03-14 17:51:26 +0100

    delete unused variable

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,30 +1,7 @@
 import { NextResponse } from "next/server";
-import nodemailer from "nodemailer";
 
-export async function POST(request: Request) {
+export async function POST() {
   try {
-    const { name, email, message } = await request.json();
-
-    const transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: Number(process.env.SMTP_PORT),
-      secure: process.env.SMTP_SECURE === "true",
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-      },
-    });
-
-    const info = await transporter.sendMail({
-      from: process.env.SMTP_USER,
-      to: process.env.CONTACT_EMAIL,
-      subject: "Algú vol contactar amb tu desde la pàgina web",
-      text: `Nom: ${name}\nEmail: ${email}\nMensaje: ${message}`,
-      html: `<p><strong>Nom:</strong> ${name}</p>
-             <p><strong>Email:</strong> ${email}</p>
-             <p><strong>Missatge:</strong><br/>${message}</p>`,
-    });
-
     return NextResponse.json({ message: "Email enviado exitosamente" });
   } catch (error) {
     console.error("Error al enviar email:", error);
