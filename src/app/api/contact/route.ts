e8b95d963d9127a4ee75d2b5a77e108455eba1a4
marsino00 <rogermarsino@gmail.com>
2025-03-18 10:50:56 +0100

    Refactoring contact form api call

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,8 +1,39 @@
+import nodemailer from "nodemailer";
 import { NextResponse } from "next/server";
 
-export async function POST() {
+export async function POST(request: Request) {
   try {
-    return NextResponse.json({ message: "Email enviado exitosamente" });
+    const { name, email, message } = await request.json();
+
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: Number(process.env.SMTP_PORT),
+      secure: process.env.SMTP_SECURE === "true",
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
+
+    await transporter.sendMail({
+      from: process.env.SMTP_USER,
+      to: process.env.CONTACT_EMAIL,
+      subject: `Formulari rebut desde la web de ${name}`,
+      text: `
+        Has rebut un missatge de contacte:
+        Nom: ${name}
+        Email: ${email}
+        Missatge: ${message}
+      `,
+      html: `
+        <p>Algú vol contactar amb vosaltres desde la pàgina web:</p>
+        <p><strong>Nom:</strong> ${name}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Missatge:</strong> ${message}</p>
+      `,
+    });
+
+    return NextResponse.json({ message: "Email enviat correctament" });
   } catch (error) {
     console.error("Error al enviar email:", error);
     return NextResponse.json(
